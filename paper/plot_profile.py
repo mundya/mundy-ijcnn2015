271ae9dc9af68607f75bd90ee4946c920294f5cc
jamie <neworderofjamie@gmail.com>
2015-01-08 20:32:45 +0000

    git failure

--- paper/plot_profile.py
+++ paper/plot_profile.py
@@ -10,9 +10,9 @@ def load_results(csv_filename):
     with open(csv_filename, "rb") as csv_file:
         return list(csv.reader(csv_file))
 
-def plot_contour(results, get_x_function, get_y_function, filter_function, title, x_label, y_label):
+def plot_contour(results, get_x_function, get_y_function, filter_function, title, x_label, y_label, filename):
      # Create single-axis figure
-    figure, axis = pylab.subplots()
+    figure, axis = pylab.subplots(figsize=(plot_settings.column_width, 3.5), frameon=False)
     
     # Generate x, y, z data
     filtered_results = [[get_x_function(r), get_y_function(r), float(r[6])] for r in results if filter_function(r)]
@@ -28,7 +28,8 @@ def plot_contour(results, get_x_function, get_y_function, filter_function, title
     contours = axis.contour(xi, yi, zi, cmap=pylab.cm.gray, levels=[0.2, 0.4, 0.6, 0.8])
     axis.clabel(contours)
 
-    axis.set_title(title)
+    if title is not None:
+        axis.set_title(title)
     axis.set_xlabel(x_label)
     axis.set_ylabel(y_label)
     
@@ -38,10 +39,14 @@ def plot_contour(results, get_x_function, get_y_function, filter_function, title
     # Use dimensions of data points for x ticks
     axis.set_xticks(unzipped_results[0])
     axis.set_xticklabels(unzipped_results[0])
+    
+    # Save figure
+    figure.tight_layout(pad=0.5)
+    figure.savefig(filename)
      
-def plot_surface(results, get_x_function, get_y_function, filter_function, title, x_label, y_label):
+def plot_surface(results, get_x_function, get_y_function, filter_function, title, x_label, y_label, filename):
     # Create 3D figure
-    figure = pylab.figure()
+    figure = pylab.figure(figsize=(plot_settings.column_width, 3.5), frameon=False)
     axis = figure.add_subplot(111, projection="3d")
     
     # Generate x, y, z data
@@ -54,14 +59,19 @@ def plot_surface(results, get_x_function, get_y_function, filter_function, title
     # Plot triangle-surface
     axis.plot_trisurf(unzipped_results[0], unzipped_results[1], unzipped_results[2], cmap = pylab.cm.coolwarm, shade = True)
     
-    axis.set_title(title)
+    if title is not None:
+        axis.set_title(title)
     axis.set_xlabel(x_label)
     axis.set_ylabel(y_label)
     axis.set_zlabel("CPU load")
     
-def plot_stack(results, get_x_function, filter_function, title, x_label):
+    # Save figure
+    figure.tight_layout(pad=0.5)
+    figure.savefig(filename)
+    
+def plot_stack(results, get_x_function, filter_function, title, x_label, filename):
     # Create single-axis figure
-    figure, axis = pylab.subplots()
+    figure, axis = pylab.subplots(figsize=(plot_settings.column_width, 3.5), frameon=False)
     
     # Filter results
     filtered_results = [[get_x_function(r), float(r[7]), float(r[8]), float(r[9])] for r in results if filter_function(r)]
@@ -82,15 +92,20 @@ def plot_stack(results, get_x_function, filter_function, title, x_label):
     figure.legend(legendProxies, ["Input filtering", "Neuron update", "Output"])
     
     # Setup axis
-    axis.set_title(title)
+    if title is not None:
+        axis.set_title(title)
     axis.set_xlabel(x_label)
     axis.set_ylabel("CPU time")
     axis.set_xlim((min(unzipped_results[0]), max(unzipped_results[0])))
     axis.set_ylim((0.0, 1.0))
+    
+    # Save figure
+    figure.tight_layout(pad=0.5)
+    figure.savefig(filename)
 
-def plot_stack_bar(results, get_x_function, filter_function, title, x_label):
+def plot_stack_bar(results, get_x_function, filter_function, title, x_label, filename):
     # Create single-axis figure
-    figure, axis = pylab.subplots()
+    figure, axis = pylab.subplots(figsize=(plot_settings.column_width, 3.5), frameon=False)
     
     # Filter results
     filtered_results = [[get_x_function(r), float(r[7]) * 100.0, float(r[8]) * 100.0, float(r[9]) * 100.0] for r in results if filter_function(r)]
@@ -106,7 +121,8 @@ def plot_stack_bar(results, get_x_function, filter_function, title, x_label):
     axis.bar(bar_x, unzipped_results[2], bottom=unzipped_results[1], width=bar_width, label="Neuron update", color="0.5")
     axis.bar(bar_x, unzipped_results[3], bottom=unzipped_results[2], width=bar_width, label="Output", color="0.75")
     
-    axis.set_title(title)
+    if title is not None:
+        axis.set_title(title)
     axis.set_xlabel(x_label)
     axis.set_ylabel("Simulation time step CPU time %")
     
@@ -115,6 +131,10 @@ def plot_stack_bar(results, get_x_function, filter_function, title, x_label):
 
     axis.legend()
     
+    # Save figure
+    figure.tight_layout(pad=0.5)
+    figure.savefig(filename)
+    
 # Load results CSV file
 results = load_results("profile_paper.csv")
 
@@ -122,8 +142,9 @@ results = load_results("profile_paper.csv")
 plot_stack_bar(results, 
     lambda r: int(r[1]),
     lambda r: int(r[0]) == 16 and int(r[2]) == 0,
-    "CPU time when simulating 16D ensembles of varying sizes",
-    "Ensemble size")
+    None,
+    "Ensemble size",
+    "comm_channel_cpu_16d_bar.pdf")
 '''
 plot_stack(results, 
     lambda r: int(r[0]),
@@ -141,8 +162,9 @@ plot_contour(results,
     lambda r: int(r[0]),
     lambda r: int(r[1]),
     lambda r: int(r[2]) == 0,
-    "CPU time when simulating neuron ensembles with varying dimensionality and size",
+    None,
     "Number of dimensions",
-    "Ensemble size")
+    "Ensemble size",
+    "comm_channel_cpu_contour.pdf")
 
 pylab.show()
